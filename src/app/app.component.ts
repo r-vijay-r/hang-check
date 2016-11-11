7dee3833080f331b2e5d7a852113192ec942584e
r-vijay-r <503r.vijay@gmail.com>
2016-11-11 23:41:17 +0530

    hangout working inside component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  email="503r.vijay@gmail.com";
+   constructor(){
+		var head= document.getElementsByTagName('head')[0];
+		var script= document.createElement('script');
+		script.type= 'text/javascript';
+		script.src= 'https://apis.google.com/js/platform.js';
+		head.appendChild(script);
+		var body= document.getElementsByTagName('body')[0];
+		var div= document.createElement('div');
+		div.id="hangout";
+		div.innerHTML='<g:hangout render="createhangout" invites="[{ id : \''+this.email+'\', invite_type : \'EMAIL\' }]"> </g:hangout>';
+		body.appendChild(div);
+
+  }
 }
