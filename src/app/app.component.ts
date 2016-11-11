034264eb18bee0ec81fb9ea633da3b9db39e18eb
r-vijay-r <503r.vijay@gmail.com>
2016-11-12 00:49:37 +0530

    User can select the email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-  email="503r.vijay@gmail.com";
+  div;
+  first:boolean=true;
+  body;
    constructor(){
+
+  }
+  setEmail(email){
+  		this.first=false;
 		var head= document.getElementsByTagName('head')[0];
 		var script= document.createElement('script');
 		script.type= 'text/javascript';
 		script.src= 'https://apis.google.com/js/platform.js';
 		head.appendChild(script);
-		var body= document.getElementsByTagName('body')[0];
-		var div= document.createElement('div');
-		div.id="hangout";
-		div.innerHTML='<g:hangout render="createhangout" invites="[{ id : \''+this.email+'\', invite_type : \'EMAIL\' }]"> </g:hangout>';
-		body.appendChild(div);
-
+		this.body= document.getElementsByTagName('body')[0];
+		this.div= document.createElement('div');
+		this.div.innerHTML='<g:hangout render="createhangout" invites="[{ id : \''+email+'\', invite_type : \'EMAIL\' }]"> </g:hangout>';
+		this.body.appendChild(this.div);
   }
 }
